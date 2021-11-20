12576c6b8dba65be8d3ea34caf50128f13d248ad
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2021-11-20 14:13:48 +0300

    C#
    Average of two numbers

---
+++ AverageOfTwoNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AverageOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter First Number: ");
+            double number1 =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter Second Number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double average=(number1+number2)/2;
+            Console.WriteLine("the Average of These Two Numbers Is: "+average);
+            Console.ReadLine();
+
+        }
+    }
+}
