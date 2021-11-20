90dbf944186f1a3d9b4d499b0bee7b8d97a53901
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2021-11-20 14:40:08 +0300

    C#
    The sum of numbers from 1 to 100

---
+++ additionExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AdditionExample
+{
+    class Program
+    {
+        //The sum of numbers from 1 to 100
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int sum = 0;
+            do
+            {           
+                sum += i;             
+                i += 1;
+
+            } while (i<=100);
+
+            Console.WriteLine("Total: "+sum);
+            Console.ReadLine();
+
+        }
+    }
+}
