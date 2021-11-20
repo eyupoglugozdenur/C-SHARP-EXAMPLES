c30ceb531e1cb0a0e0076f32f873c4d000fcba91
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2021-11-20 15:05:09 +0300

    C#
    The multiplication table

---
+++ MultiplicationTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        //The multiplication table
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i++)
+            {
+             
+                for(int j=1;j<=10;j++)
+                {
+                 
+                    int product = j * i;
+                    Console.WriteLine(+i +" x " +j +" = " +product);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
